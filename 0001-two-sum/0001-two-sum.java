e48d53ca20e2fbde47a6b44202ac5768a54c2cbc
Sashwath P <sashwathprakash725@gmail.com>
2026-06-23 11:04:33 +0530

    Time: 51 ms (5.91%), Space: 47.1 MB (39.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,8 +4,8 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+nums[j]==target){
-                    result[0]+=i;
-                    result[1]+=j;
+                    result[0]=i;
+                    result[1]=j;
                 }
             }
         }
