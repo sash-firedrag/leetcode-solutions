633f9f8dc2b8ba0b1e8686e017b19cae2964ec75
Sashwath P <sashwathprakash725@gmail.com>
2026-07-23 14:03:26 +0530

    Time: 1 ms (98.43%), Space: 80.3 MB (81.61%) - LeetHub - Time: O(n + m), Space: O(1)

---
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int i=0,j=0;
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else if(nums1[i]>nums2[j]){
+                j++;
+            }
+            else{
+                return nums1[i];
+            }
+        }
+        return -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
