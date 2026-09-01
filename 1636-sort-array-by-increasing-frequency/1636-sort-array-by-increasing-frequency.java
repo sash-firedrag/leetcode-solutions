934bd0ff261e5e901d19daa09018e18af39f2eb7
Sashwath P <sashwathprakash725@gmail.com>
2026-09-01 20:17:00 +0530

    Time: 7 ms (75.68%), Space: 45.6 MB (64.62%) - Time: O(n log n), Space: O(n)

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }
+            else{
+                map.put(nums[i],1);
+            }
+        }
+        List<Integer> list = new ArrayList<Integer>(map.keySet());
+        Collections.sort(list, (a, b) -> {
+            if (map.get(a) != map.get(b)) {
+                return map.get(a) - map.get(b);
+            }
+            return b - a;
+        });
+        int[] result = new int[nums.length];
+        int index = 0;
+
+        for (int num : list) {
+            for (int i = 0; i < map.get(num); i++) {
+                result[index++] = num;
+            }
+        }
+
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
