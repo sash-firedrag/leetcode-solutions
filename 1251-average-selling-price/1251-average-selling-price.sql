47836d1fe6492fd31a6b795cab1017f263593824
Sashwath P <sashwathprakash725@gmail.com>
2026-09-08 14:24:43 +0530

    Time: 909 ms (51.29%), Space: 0B (100%) -Time: O(N * M), Space: O(N)

---
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT p.product_id,COALESCE(ROUND(SUM(p.price*u.units)/SUM(u.units),2),0) AS 'average_price'
+FROM Prices p
+LEFT JOIN UnitsSold u
+ON p.product_id=u.product_id
+AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
